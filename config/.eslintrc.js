60a55045c266f8659f76d80ba00f4ababac1059f
Martin Metodiev <metodiev.martin@gmail.com>
2019-09-11 17:33:12 +0300

    Initial version

---
+++ config/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+  globals: {
+    $nuxt: true
+  }
+};
