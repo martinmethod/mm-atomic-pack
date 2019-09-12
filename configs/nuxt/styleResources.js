fc6c771de306f1fc05ea98d088dc1f16402ebc6d
Martin Metodiev <metodiev.martin@gmail.com>
2019-09-12 11:59:28 +0300

    Add Nuxt config files for styles

---
+++ configs/nuxt/styleResources.js
@@ -0,0 +1,23 @@
+import { name } from '../../package.json'
+
+const resourcesPath = 'assets/styles/resources';
+const atomicPath = `node_modules/${name}/${resourcesPath}`;
+const localPath = `~${resourcesPath}`;
+
+export default {
+  scss: [
+    `${localPath}/base.scss`,
+    `${atomicPath}/functions/**/*.scss`,
+    `${localPath}/functions/**/*.scss`,
+    `${atomicPath}/data/swatches/primary/**/*.scss`,
+    `${localPath}/data/swatches/primary/**/*.scss`,
+    `${atomicPath}/data/swatches/secondary/**/*.scss`,
+    `${localPath}/data/swatches/secondary/**/*.scss`,
+    `${atomicPath}/data/roles/**/*.scss`,
+    `${localPath}/data/roles/**/*.scss`,
+    `${atomicPath}/mixins/**/*.scss`,
+    `${localPath}/mixins/**/*.scss`,
+    `${atomicPath}/placeholders/**/*.scss`,
+    `${localPath}/placeholders/**/*.scss`
+  ]
+};
